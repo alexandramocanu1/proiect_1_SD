65ca18813b71cffda74471517cf98a115939c975
alexandramocanu1 <119112644+alexandramocanu1@users.noreply.github.com>
2023-03-19 18:59:49 +0200

    Update genereaza-aleatoriu.cpp

--- genereaza-aleatoriu.cpp
+++ genereaza-aleatoriu.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
-#include <random>
+#include <cstdlib> // biblioteca pentru functia rand()
 
-int main()
-{
+using namespace std;
+
+int main() {
     int n, m;
-    std::cout << "Introduceti numarul de numere aleatorii (n): ";
-    std::cin >> n;
-    std::cout << "Introduceti valoarea maxima (m): ";
-    std::cin >> m;
-
-    // se initializeaza generatorul cu o valoare aleatoare initiala
-    std::random_device rd;
-    std::mt19937 gen(rd());
-
-    // se creeaza o distributie uniforma intre 0 si m-1
-    std::uniform_int_distribution<> dis(0, m-1);
-
-    // se genereaza n numere aleatoare si se afiseaza
-    for (int i = 0; i < n; ++i) {
-        std::cout << dis(gen) << " ";
+    cout << "Introduceti numarul de valori si valoarea maxima: ";
+    cin >> n >> m;
+    
+    for (int i = 0; i < n; i++) {
+        int numar_aleatoriu = rand() % m;
+        cout << numar_aleatoriu << " ";
     }
-
-    std::cout << "\n";
+    cout << endl;
     return 0;
 }
